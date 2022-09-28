8cfc208e28965bb0b20ec66a86cbd0d3694ea026
Ivan Ustyantsev <vangold915@gmail.com>
2022-09-28 20:34:34 +0700

    Fixed bug in PrintArray function

--- task29/Program.cs
+++ task29/Program.cs
@@ -27,8 +27,7 @@ int[] CreateArray() {
 
 void PrintArray(int[] arr) {
     for (int i = 0; i < array.Length; i++) {
-        if 
-            (i == 0) Console.Write($"[{array[i]}, ");
+        if (i == 0) Console.Write($"[{array[i]}, ");
         else if (i == array.Length - 1) 
             Console.Write($"{array[i]}]");
         else 
