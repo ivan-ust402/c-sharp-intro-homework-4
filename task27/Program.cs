79e0d215277a557c1d35c5caa2ade63d289d70a1
Ivan Ustyantsev <vangold915@gmail.com>
2022-09-28 18:18:21 +0700

    Fixed Sum function and completed task 27

--- task27/Program.cs
+++ task27/Program.cs
@@ -11,11 +11,18 @@
 - Должна быть отдельная функция, которая возвращает результат суммы цифр
 - Внутри функций НЕЛЬЗЯ использовать класс Console. Только в основном блоке кода
 */
+Console.Clear();
+Console.WriteLine("Введите число, чтобы получить сумму цифр, из которых состоит введенное число: ");
+int number = int.Parse(Console.ReadLine()!);
+
+int amount = Sum(number);
+Console.WriteLine($"Сумма цифр числа: {amount}");
 
 int Sum (int number) {
     int sum = 0;
     while (number != 0) {
-        remainder = number % 10;
+        if (number < 0) number = -number;
+        int remainder = number % 10;
         number = number / 10;
         sum = sum + remainder;
     }
